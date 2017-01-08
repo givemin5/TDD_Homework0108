7d7f096a089308a0d15e01ccd1294a9881db93a6
givemin5 <givemin5@gmail.com>
2017-01-08 23:03:40 +0800

    Sample 1 :  3筆一組，取Cost總和 預計的樣子

--- Homework0108.Tests/UnitTest1.cs
+++ Homework0108.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Homework0108.Tests
 {
@@ -7,9 +8,30 @@ namespace Homework0108.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Products使用測試資料三筆一組取Cost總和應為6_15_24_21()
         {
+            var products = new List<Product>();
 
+            int[] expected = new int[] { 6, 15, 24, 21 };
+
+            int[] actual = products.GroupSum(x => x.Cost, 3);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenne { get; set; }
+        public int SellPrice { get; set; }
+    }
+
+    public static class ListExtension
+    {
+        public static int[] GroupSum(this List<Product> products, Func<Product, int> selector, int group) {
+            return new int[1];
         }
     }
 }
